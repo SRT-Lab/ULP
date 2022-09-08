01fc00941d3ff5ec30987b081b477834561cdef2
isedki <isedki@gmail.com>
2022-09-08 11:50:20 -0400

    Update ULP_exec.py

--- ULP_exec.py
+++ ULP_exec.py
@@ -9,6 +9,7 @@ sys.path.append('../')
 from ULP_benchmark import benchmark_settings
 from common import datasets, common_args
 from exec_main import evaluator, prepare_results
+from ULP import LogParser
 
 input_dir = './input/'  # The input directory of log file
 output_dir = './output'  # The output directory of parsing result
@@ -36,7 +37,7 @@ if __name__ == "__main__":
             input_dir=input_dir,
             output_dir=output_dir,
             log_file=log_file,
-            LogParser=ULP.LogParser,
+            LogParser=LogParser,
             param_dict={
                 'log_format': setting['log_format'], 'indir': indir, 'outdir': output_dir, 'rex': setting['regex']
             },
